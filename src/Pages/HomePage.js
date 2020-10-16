ae15c18e6bc1785dbf16e652957fd33c42adbea7
John <johnlee46@gmail.com>
2020-10-15 20:33:18 -0700

    added github and linkedin
    added github and linkedin

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Examples from '../Components/Examples'
 import Carousel from '../Components/Carousel';
+import github from '../Assets/images/github.png';
+import linkedin from '../Assets/images/linkedin.png';
 function HomePage(props){
     return (
         <div>
+            <a href="https://www.github.com/johnlee46"><img class="links" src={github}></img></a>
+            
+            <a href="https://www.linkedin.com/in/johnlee46">
+            <img class="links" src={linkedin}></img>
+                </a>
         <Examples title={props.title}/>
         <Carousel />
         </div>
