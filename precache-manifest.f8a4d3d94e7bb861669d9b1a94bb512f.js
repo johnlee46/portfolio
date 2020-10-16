e1ca39d2153f747841cd133a25a7a0ef0b60a606
John <johnlee46@gmail.com>
2020-10-15 20:38:02 -0700

    Updates

---
+++ precache-manifest.f8a4d3d94e7bb861669d9b1a94bb512f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a139e960876b8c5f619980cc178ea36",
+    "url": "/myfirstreact/index.html"
+  },
+  {
+    "revision": "4d7f20a27fcc0b9c72c6",
+    "url": "/myfirstreact/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "ec66e5521d3c78c33633",
+    "url": "/myfirstreact/static/css/main.801fd1c6.chunk.css"
+  },
+  {
+    "revision": "4d7f20a27fcc0b9c72c6",
+    "url": "/myfirstreact/static/js/2.236ce851.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myfirstreact/static/js/2.236ce851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec66e5521d3c78c33633",
+    "url": "/myfirstreact/static/js/main.5cf9f72e.chunk.js"
+  },
+  {
+    "revision": "17c9c731ff8ecbbf2ada",
+    "url": "/myfirstreact/static/js/runtime-main.2fefc55a.js"
+  },
+  {
+    "revision": "8f7d1e43deb599f59bef5dc795f9b600",
+    "url": "/myfirstreact/static/media/github.8f7d1e43.png"
+  },
+  {
+    "revision": "56812ac3cb729b8eeea39ad23bba30d5",
+    "url": "/myfirstreact/static/media/imageone.56812ac3.png"
+  },
+  {
+    "revision": "6a44bd3dcedb48126c43880f876aae80",
+    "url": "/myfirstreact/static/media/imagethree.6a44bd3d.png"
+  },
+  {
+    "revision": "c9758fe190ec8e805dde51cbd3fe75cd",
+    "url": "/myfirstreact/static/media/imagetwo.c9758fe1.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myfirstreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
